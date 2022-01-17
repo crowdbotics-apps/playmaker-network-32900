2c2a2496f746c59b1e0b38077d7f3ebf0f53381c
Crowdbotics <team@crowdbotics.com>
2022-01-17 06:25:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "playmaker_network_32900",
+  url: "https://playmaker_network_32900.botics.co",
+  api: "https://playmaker_network_32900.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
